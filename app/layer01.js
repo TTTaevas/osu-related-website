18de81d6470bb697da6c17e48dfe2e0f4813155e
Taevas <67872932+TTTaevas@users.noreply.github.com>
2022-02-08 15:58:43 +0100

    Create matches

--- app/layer01.js
+++ app/layer01.js
@@ -343,6 +343,123 @@ router.route("/qualifiers-results")
 	res.redirect("/layer01/qualifiers-results")
 })
 
+router.route("/matches")
+.get(async (req, res) => {
+	let check = await userCheck(client, req.session.user)
+	let brackets_col = check.db.collection("brackets")
+	let brackets = await brackets_col.find().toArray()
+
+	res.status(200).render("layer01/matches", {user: check.user, brackets: brackets})
+})
+.post(async (req, res) => {
+	if (!req.body) {return res.status(403).render("layer01/error", {status: {code: 403, reason: "Unauthorized; you shouldn't be there :3c"}})}
+	var check
+	var brackets_col
+	var brackets
+	var mode
+
+	switch(req.body.act) {
+		case "create":
+			check = await userCheck(client, req.session.user, "admin")
+			if (!check.authorized) {return res.status(403).render("layer01/error", {status: {code: 403, reason: "Unauthorized; you shouldn't be there :3c"}})}
+			brackets_col = check.db.collection("brackets")
+
+			let bracket = {
+				name: req.body.c_bracket_name,
+				matches: []
+			}
+
+			// There should be no case in which I'd make a bracket with only 1 match but just in case
+			if (!Array.isArray(req.body.c_id)) {return res.status(403).render("layer01/error", {status: {code: 403, reason: "Only 1 match?"}})}
+			for (let i = 0; i < req.body.c_id.length; i++) {
+				// date format in input should be mm-dd hh:mm
+				let month = Number(req.body.c_time[i].substring(0, 2)) - 1
+				let day = req.body.c_time[i].substring(3, 5)
+				let hour = req.body.c_time[i].substring(6, 8)
+				let minutes = req.body.c_time[i].substring(9, 11)
+				let date = new Date(Date.UTC(2022, month, day, hour, minutes)) // yes it uses both integers and strings
+
+				// worse code
+				let player1 = check.users.find((user) => {return user.username.toLowerCase() == req.body.c_player_1[i].toLowerCase()})
+				player1 = player1 ? {id: player1.id, username: player1.username} : {id: 0, username: "error"}
+				let player2 = check.users.find((user) => {return user.username.toLowerCase() == req.body.c_player_2[i].toLowerCase()})
+				player2 = player2 ? {id: player2.id, username: player2.username} : {id: 0, username: "error"}
+
+				let match = {
+					id: Number(req.body.c_id[i]),
+					players: [player1, player2],
+					scores: [0, 0],
+					schedule: date,
+					type: req.body.c_type[i].toLowerCase(),
+					referee: false,
+					streamer: false,
+					mp_link: false
+				}
+				bracket.matches.push(match)
+			}
+			
+			await brackets_col.insertOne(bracket)
+			console.log(`Created ${req.body.c_bracket_name}!`)
+			return res.redirect("/layer01/matches")
+			break
+		case "ref_add":
+		case "str_add":
+			mode = req.body.act == "ref_add" ? "ref" : "str"
+			check = mode == "ref" ? await userCheck(client, req.session.user, "referee") : await userCheck(client, req.session.user, "streamer")
+			if (!check.authorized) {return res.status(403).render("layer01/error", {status: {code: 403, reason: "Unauthorized; you shouldn't be there :3c"}})}
+			brackets_col = check.db.collection("brackets")
+			brackets = await brackets_col.find().toArray()
+
+			let taken_matches = req.body.matches.replace(/ /g, "").split(",")
+			for (let i = 0; i < taken_matches.length; i++) {
+				let bracket_to_add_to = brackets.find((b) => {return b.matches.findIndex((m) => {return m.id == taken_matches[i]}) != -1})
+				let matches_arr = bracket_to_add_to.matches
+				let index = matches_arr.findIndex((m) => {return m.id == taken_matches[i]})
+
+				if (index != -1) {
+					if (mode == "ref") {
+						matches_arr[index].referee = {id: check.user.id, name: check.user.username}
+					} else {
+						matches_arr[index].streamer = {id: check.user.id, name: check.user.username}
+					}
+				}
+				
+				let a = await brackets_col.updateOne({name: bracket_to_add_to.name}, {$set: {matches: matches_arr}})
+				if (a.modifiedCount) {console.log(`${taken_matches[i]} is now being ${mode == "ref" ? "reffed" : "streamed"} by ${check.user.username}`)}
+			}
+			return res.redirect("/layer01/matches")
+			break
+		case "ref_rem":
+		case "str_rem":
+			mode = req.body.act == "ref_rem" ? "ref" : "str"
+			check = mode == "ref" ? await userCheck(client, req.session.user, "referee") : await userCheck(client, req.session.user, "streamer")
+			if (!check.authorized) {return res.status(403).render("layer01/error", {status: {code: 403, reason: "Unauthorized; you shouldn't be there :3c"}})}
+			brackets_col = check.db.collection("brackets")
+			brackets = await brackets_col.find().toArray()
+
+			let dropped_matches = req.body.matches.replace(/ /g, "").split(",")
+			for (let i = 0; i < dropped_matches.length; i++) {
+				let bracket_to_remove_from = brackets.find((b) => {return b.matches.findIndex((m) => {return m.id == dropped_matches[i]}) != -1})
+				let matches_arr = bracket_to_remove_from.matches
+				let index = matches_arr.findIndex((m) => {return m.id == dropped_matches[i]})
+
+				if (index != -1) {
+					if (mode == "ref") {
+						matches_arr[index].referee = false
+					} else {
+						matches_arr[index].streamer = false
+					}
+				}
+				
+				let a = await brackets_col.updateOne({name: bracket_to_remove_from.name}, {$set: {matches: matches_arr}})
+				if (a.modifiedCount) {console.log(`${dropped_matches[i]} has been dropped ${mode == "ref" ? "(referee)" : "(streamer)"} by ${check.user.username}`)}
+			}
+			return res.redirect("/layer01/matches")
+			break
+	}
+	res.redirect("/layer01/matches")
+})
+
 
 // 404
 router.get("/*", (req, res) => {
