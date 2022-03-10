5611ec522850a231cb51a7ac8d66d0846afab1d6
Taevas <67872932+TTTaevas@users.noreply.github.com>
2022-03-10 03:30:33 +0100

    Database change: Delete all unused stuff

--- db-clients.js
+++ db-clients.js
@@ -25,9 +25,10 @@ const { MongoClient } = require("mongodb");
 
 
 // This function updates the database automatically to the new structure used since approx commit 394292b0
-// Note that it doesn't *delete* anything of the old structure (FOR NOW), so code before 394292b0 would still work fine
 
 async function dbUpdater(c) {
+	let safe_to_delete = true
+
 	// At first, the history part of the website was supposed to use two separate databases, one for referee, one for player
 	// Now they share the same database, meaning referee collections need a `r_` at the start of their name
 	// Player history actually didn't exist, but they will have a `p_` at the start of their name
@@ -41,6 +42,7 @@ async function dbUpdater(c) {
 			let old_col = await history.collection(`${h_names[i]}`).find().toArray()
 			let insertion = await history.collection(`r_${h_names[i]}`).insertMany(old_col)
 			console.log(`(TOURNAMENT HISTORY | REFEREE) ${insertion.insertedCount}/${old_col.length} ${h_names[i]} now using the new database structure!`)
+			if (insertion.insertedCount < old_col.length) {safe_to_delete = false}
 		}
 	}
 
@@ -58,6 +60,7 @@ async function dbUpdater(c) {
 			let old_col = await auth.collection(`${l01_names[i]}`).find().toArray()
 			let insertion = await layer01.collection(`${l01_names[i]}`).insertMany(old_col)
 			console.log(`(LAYER01) ${insertion.insertedCount}/${old_col.length} ${l01_names[i]} now using the new database structure!`)
+			if (insertion.insertedCount < old_col.length) {safe_to_delete = false}
 		}
 	}
 
@@ -78,11 +81,12 @@ async function dbUpdater(c) {
 			if (insertion.insertedId) {counter++}
 		}
 		console.log(`(TOURNAMENT HISTORY) ${counter}/${users.length} users now using the new authorization system!`)
+		if (counter < users.length) {safe_to_delete = false}
 	}
 
 	/// referee
-	let l_roles = await layer01.collection("roles").find().toArray()
-	if (!l_roles.length) {
+	let l01_roles = await layer01.collection("roles").find().toArray()
+	if (!l01_roles.length) {
 		let users = await auth.collection("users").find().toArray()
 		let counter = 0
 		for (let i = 0; i < users.length; i++) {
@@ -93,8 +97,40 @@ async function dbUpdater(c) {
 			if (insertion.insertedId) {counter++}
 		}
 		console.log(`(LAYER01) ${counter}/${users.length} users now using the new authorization system!`)
+		if (counter < users.length) {safe_to_delete = false}
 	}
 
-	return "Database updater finished to run, now running server!"
+	// Now in theory, it is safe to clean the database by deleting the stuff
+	// used in the old code but not used in the new code anymore
+	if (!safe_to_delete) {return `\nEven though the database updater finished to run, it was not able to update everything...
+Make sure to check out what went wrong before running the server/database updater again!`}
+	
+	/// Drop/Delete the old tournament history referee collections
+	let h_deleted = []
+	for (let i = 0; i < h_names.length; i++) {try{
+		let deletion = await history.collection(h_names[i]).drop()
+		if (deletion) {h_deleted.push(h_names[i])}
+	} catch {console.log(`(TOURNAMENT HISTORY) Could not delete ${h_names[i]}`)}}
+	if (h_deleted.length) {console.log(`(TOURNAMENT HISTORY) ${h_deleted} collections have been deleted!`)}
 
+	/// Drop/Delete the old LAYER01 collections in the auth database
+	let l01_deleted = []
+	for (let i = 0; i < l01_names.length; i++) {try{
+		let deletion = await auth.collection(l01_names[i]).drop()
+		if (deletion) {l01_deleted.push(l01_names[i])}
+	} catch {console.log(`(LAYER01 auth db) Could not delete ${l01_names[i]}`)}}
+	if (l01_deleted.length) {console.log(`(LAYER01 auth db) ${l01_deleted} collections have been deleted!`)}
+
+	// Strip user objects of their legacy LAYER01 roles
+	let users_stripped_count = 0
+	let users_not_stripped_count = 0
+	let users = await auth.collection("users").find().toArray()
+	for (let i = 0; i < users.length; i++) {if (users[i].roles) {
+		delete users[i].roles
+		let stripping = await auth.collection("users").replaceOne({_id: users[i]._id}, users[i])
+		if (stripping.modifiedCount) {users_stripped_count++}
+	} else {users_not_stripped_count++}}
+	console.log(`(auth db) Stripped ${users_stripped_count}/${users.length} of their legacy LAYER01 roles! (${users_not_stripped_count} users didn't have them)`)
+
+	return "Database updater finished to run, now running server!"
 }
