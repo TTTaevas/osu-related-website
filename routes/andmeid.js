a95d7b0ba5e06782d20afd0519e7c8abd7bc1587
Taevas <67872932+TTTaevas@users.noreply.github.com>
2022-03-22 02:12:00 +0100

    [ANDMEID] Choose style and create error page

--- routes/andmeid.js
+++ routes/andmeid.js
@@ -4,4 +4,8 @@ const router = express.Router()
 const home = require("../controllers/andmeid/home")
 router.get("/", home.main)
 
+router.get("/*", (req, res) => {
+	res.status(404).render("andmeid/error", {error: {code: 404, message: "The content you're looking for does not exist"}})
+})
+
 module.exports = router
